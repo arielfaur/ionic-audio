4fd6ea4127242e2683c723615f198610a21d60c3
Ariel Faur <a.faur@diamonddogs.cc>
2015-04-30 21:48:17 -0300

    optimized icons and performance

--- example-audio/www/js/ion-audio.js
+++ example-audio/www/js/ion-audio.js
@@ -22,9 +22,8 @@ angular.module('ionic-audio', ['ionic'])
             },
             controller: ['$scope', function($scope) {
 
-                var media, playerStatus = 0, playerTimer, controller = this;
+                var media, playerTimer, controller = this;
 
-                $scope.track.hasLoaded = false;
                 $scope.track.status = 0;
                 $scope.track.duration = -1;
                 $scope.track.progress = 0;
@@ -71,27 +70,28 @@ angular.module('ionic-audio', ['ionic'])
                     }
                 };
                 this.playbackSuccess = function() {
-                    $scope.track.hasLoaded = false;
                     $scope.track.status = 0;
-                    //$scope.track.duration = -1;
                     $scope.track.progress = 0;
 
                     controller.stopTimer();
                     controller.releaseMedia();
                 };
                 this.statusChange = function(status) {
-                    // track has finished loading!
-                    if (!$scope.track.hasLoaded && status == Media.MEDIA_RUNNING) {
-                        $scope.track.hasLoaded = true;
-                        $scope.$broadcast('ionic-audio:trackLoaded');
-                    }
-
                     $scope.track.status = status;
 
                     $scope.$broadcast('ionic-audio:statusChange', status);
                 };
                 this.play = function() {
-                    if (media) {
+                    $ionicPlatform.ready(function() {
+                        if (!window.cordova && !window.Media) {
+                            console.log("ionic-audio: missing Cordova Media plugin. Have you installed the plugin? \nRun 'ionic plugin add org.apache.cordova.media'");
+                            return;
+                        }
+
+                        if (!media) {
+                            controller.init();
+                        }
+
                         if ($scope.track.status == Media.MEDIA_RUNNING) {
                             console.log('ionic-audio: pausing...');
                             media.pause();
@@ -101,8 +101,8 @@ angular.module('ionic-audio', ['ionic'])
                             media.play();
                             controller.startTimer();
                         }
+                    }, false);
 
-                    }
                 }
             }],
             link: function(scope, element, attrs, controller) {
@@ -112,21 +112,11 @@ angular.module('ionic-audio', ['ionic'])
                     return;
                 }
 
-                $ionicPlatform.ready(function() {
-                    if (!window.cordova && !window.Media) {
-                        console.log("ionic-audio: missing Cordova Media plugin. Have you installed the plugin? \nRun 'ionic plugin add org.apache.cordova.media'");
-                        return;
-                    }
-
-                    controller.init();
-                }, false);
-
                 scope.$on('$destroy', function() {
                     // Make sure that the interval is destroyed too
                     controller.stopTimer();
                     controller.releaseMedia();
                 });
-
 
             }
         }
@@ -134,43 +124,47 @@ angular.module('ionic-audio', ['ionic'])
     .directive('ionAudioPlay', ['$ionicPlatform', function($ionicPlatform) {
         return {
             restrict: 'EA',
-            scope: true,
-            template: '<a class="button button-icon icon"><ion-spinner icon="ios" class="ng-hide"></ion-spinner></a>',
+            scope: {
+                buttonStyles: '@'
+            },
+            template: '<a class="button button-icon icon" ng-class="buttonStyles"></a><ion-spinner icon="ios" class="ng-hide"></ion-spinner>',
             require: '^^ionAudioPlayer',
             link: function(scope, element, attrs, controller) {
                 var
-                    playControl = element.find('a');
+                    playElem = element.find('a'), spinnerElem = element.find('ion-spinner');
 
-                playControl.addClass(attrs.playIcon);
-                element.find('svg').css('height', '20px').css('width', '20px');
+                spinnerElem.css({position: 'relative', top: '8px', left: '8px;'});
 
-                var toggleSpinner = function() {
-                    element.find('ion-spinner').toggleClass('ng-hide');
-                };
+                function toggleSpinner() {
+                    spinnerElem.toggleClass('ng-hide');
+                }
 
-                element.on('click', function() {
-                    // call main directive's play method
-                    controller.play();
-                });
+                function togglePlayButton() {
+                    playElem.toggleClass('ion-play ion-pause');
+                }
 
                 $ionicPlatform.ready(function() {
-                    scope.$on('ionic-audio:trackLoaded', function () {
 
-                        toggleSpinner();
-                        playControl.addClass(attrs.pauseIcon);
+                    element.on('click', function() {
+                        togglePlayButton();
 
+                        // call main directive's play method
+                        controller.play();
                     });
+
                     scope.$on('ionic-audio:statusChange', function (e, status) {
+
                         switch (status) {
                             case Media.MEDIA_STARTING:
-                                playControl.removeClass(attrs.playIcon);
                                 toggleSpinner();
                                 break;
-                            case Media.MEDIA_PAUSED:
-                                playControl.removeClass(attrs.pauseIcon).addClass(attrs.playIcon);
-                                break;
+                            //case Media.MEDIA_PAUSED:
+                            //    break;
                             case Media.MEDIA_RUNNING:
-                                playControl.addClass(attrs.pauseIcon).removeClass(attrs.playIcon);
+                                toggleSpinner();
+                                break;
+                            case Media.MEDIA_STOPPED:
+                                togglePlayButton();
                                 break;
                         }
                     });
