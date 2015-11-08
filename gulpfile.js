f6d0e2d09e1784f48ae380c864fc95717d353384
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-08 13:38:39 -0300

    Fixing dist file names

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var rename = require('gulp-rename');
 
 gulp.task('concat', function() {
   gulp.src(['src/*.js', 'src/**/*.js'])
-    .pipe(concat('ionic-audio.js'))
+    .pipe(concat('ion-audio.js'))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('compress', function() {
-  gulp.src('dist/ionic-audio.js')
+  gulp.src('dist/ion-audio.js')
     .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('dist'));
