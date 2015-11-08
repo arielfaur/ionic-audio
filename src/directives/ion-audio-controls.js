b14293574fcdf639a7fb39bc8ed299e41d1d2648
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-08 10:59:19 -0300

    Creating ionAudioControls directive and controller files

---
+++ src/directives/ion-audio-controls.js
@@ -0,0 +1,18 @@
+angular.module('ion-audio').directive('ionAudioControls', ionAudioControls);
+
+function ionAudioControls() {
+    return {
+      restrict: 'EA',
+      scope: {},
+      require: ['ionAudioControls', '^^ionAudioTrack'],
+      controller: 'ionAudioControlsCtrl',
+      link: link
+    }
+
+    function link(scope, element, attrs, controllers) {
+        var ionAudioTrackCtrl = controllers[1];
+        controllers[0].play = ionAudioTrackCtrl.play;
+
+        scope.track = ionAudioTrackCtrl.getTrack();
+    }
+}
