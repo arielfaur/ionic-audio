3ac7b429f2c9ef91257cac1624fbfd1a43c3adb0
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-08 11:00:17 -0300

    Creating ionAudioDuration directive file

---
+++ src/directives/ion-audio-duration.js
@@ -0,0 +1,11 @@
+angular.module('ionic-audio').directive('ionAudioDuration', ionAudioDuration);
+
+function ionAudioDuration() {
+    return {
+        restrict: 'E',
+        scope: {
+            track: '='
+        },
+        template: '{{track.duration | duration}}'
+    }
+}
