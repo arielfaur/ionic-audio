6707ee6aae5420f8d25f6964259d1e40fdcb32eb
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-08 11:46:35 -0300

    Removing unecessary stuffs

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/ionic/js/ionic.bundle.js',
+      'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'src/**/*.js',
       'dist/ion-audio.js',
