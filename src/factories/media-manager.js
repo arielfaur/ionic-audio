aa9574704ec37466c2a0d9ada5193cb0a919f295
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-08 10:57:48 -0300

    Creating MediaManager file, also refactoring some function calls to simplify the code

---
+++ src/factories/media-manager.js
@@ -0,0 +1,197 @@
+angular('ionic-audio').factory('MediaManager', MediaManager);
+
+function MediaManager($interval, $timeout, $window) {
+    var tracks = [], currentTrack, currentMedia, playerTimer;
+
+    if (!$window.cordova && !$window.Media) {
+        console.log("ionic-audio: missing Cordova Media plugin. Have you installed the plugin? \nRun 'ionic plugin add org.apache.cordova.media'");
+        return null;
+    }
+
+    return {
+        add: add,
+        play: play,
+        pause: pause,
+        seekTo: seekTo,
+        destroy: destroy
+    };
+
+    /*
+    Creates a new Media from a track object
+
+     var track = {
+         url: 'https://s3.amazonaws.com/ionic-audio/Message+in+a+bottle.mp3',
+         artist: 'The Police',
+         title: 'Message in a bottle',
+         art: 'img/The_Police_Greatest_Hits.jpg'
+     }
+     */
+    function add(track, playbackSuccess, playbackError, statusChange, progressChange) {
+        if (!track.url) {
+            console.log('ionic-audio: missing track url');
+            return;
+        }
+        angular.extend(track, {
+            onSuccess: playbackSuccess,
+            onError: playbackError,
+            onStatusChange: statusChange,
+            onProgress: progressChange,
+            status: 0,
+            duration: -1,
+            progress: 0
+        });
+
+        track.id  = tracks.push(track) - 1;
+        return track.id;
+    }
+
+
+    function play(trackID) {
+        // avoid two tracks playing simultaneously
+        if (currentTrack) {
+            if (currentTrack.id == trackID) {
+                if (currentTrack.status == Media.MEDIA_RUNNING) {
+                    pause();
+                } else {
+                    //if (currentTrack.status == Media.MEDIA_PAUSED) {
+                        resume();
+                    //}
+                }
+                return;
+            } else {
+                if (currentTrack.id > -1) {
+                    stop();
+                }
+            }
+        }
+
+        $timeout(function() {
+            playTrack(tracks[trackID]);
+        }, 300);
+    }
+
+    function pause() {
+        if(!currentTrack) {
+            return;
+        }
+        console.log('ionic-audio: pausing track '  + currentTrack.title);
+
+        currentMedia.pause();
+        stopTimer();
+    }
+
+    function seekTo(pos) {
+        if (!currentMedia) return;
+
+        currentMedia.seekTo(pos * 1000);
+    }
+
+    function destroy() {
+        stopTimer();
+        releaseMedia();
+    }
+
+
+    function playTrack(track) {
+        if(!currentTrack) {
+            return;
+        }
+        currentTrack = track;
+
+        console.log('ionic-audio: playing track ' + currentTrack.title);
+
+        currentMedia = createMedia(currentTrack);
+        currentMedia.play();
+
+        startTimer();
+    }
+
+    function resume() {
+        console.log('ionic-audio: resuming track ' + currentTrack.title);
+        currentMedia.play();
+        startTimer();
+    }
+
+    function stop() {
+        console.log('ionic-audio: stopping track ' + currentTrack.title);
+        currentMedia.stop();    // will call onSuccess...
+        currentTrack = undefined;
+    }
+
+    function createMedia(track) {
+        if (!track.url) {
+            console.log('ionic-audio: missing track url');
+            return undefined;
+        }
+
+        return new Media(track.url,
+            angular.bind(track, onSuccess),
+            angular.bind(track, onError),
+            angular.bind(track, onStatusChange));
+    }
+
+    function releaseMedia() {
+        if (angular.isDefined(currentMedia)) {
+            currentMedia.release();
+            currentMedia = undefined;
+            currentTrack = undefined;
+        }
+    }
+
+    function onSuccess() {
+        stopTimer();
+        releaseMedia();
+
+        if (angular.isFunction(this.onSuccess))
+            this.onSuccess();
+    }
+
+    function onError() {
+        if (angular.isFunction(this.onError))
+            this.onError();
+    }
+
+    function onStatusChange(status) {
+        this.status = status;
+
+        if (angular.isFunction(this.onStatusChange))
+            this.onStatusChange(status);
+    }
+
+    function stopTimer() {
+        if (angular.isDefined(playerTimer)) {
+            $interval.cancel(playerTimer);
+            playerTimer = undefined;
+        }
+    }
+
+    function startTimer() {
+        if ( angular.isDefined(playerTimer) ) return;
+
+        if (!currentTrack) return;
+
+        playerTimer = $interval(function() {
+            if ( currentTrack.duration < 0){
+                currentTrack.duration = currentMedia.getDuration();
+            }
+
+            currentMedia.getCurrentPosition(
+                // success callback
+                function(position) {
+                    if (position > -1) {
+                        currentTrack.progress = position;
+                    }
+                },
+                // error callback
+                function(e) {
+                    console.log("Error getting pos=" + e);
+                });
+
+            if (angular.isFunction(currentTrack.onProgress))
+                currentTrack.onProgress(currentTrack.progress, currentTrack.duration);
+
+        }, 1000);
+    }
+}
+
+MediaManager.$inject = ['$interval', '$timeout', '$window'];
