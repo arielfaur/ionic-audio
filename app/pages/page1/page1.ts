171157d8bad945bec07a3d74ab4a68d3693fd9c8
Ariel Faur <ariel.faur@gmail.com>
2016-04-19 20:57:01 -0300

    improved examples

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -15,24 +15,29 @@ export class Page1 {
   selectedTrack: number;
    
   constructor(private _audioProvider: WebAudioProvider) {
+    
+    // plugin won't preload data by default, unless preload property is defined within json object - defaults to 'none'
     this.myTracks = [{
       src: 'https://archive.org/download/JM2013-10-05.flac16/V0/jm2013-10-05-t12-MP3-V0.mp3',
       artist: 'John Mayer',
       title: 'Why Georgia',
-      art: 'img/johnmayer.jpg'
+      art: 'img/johnmayer.jpg',
+      //preload: 'none'
     },
     {
       src: 'https://archive.org/download/JM2013-10-05.flac16/V0/jm2013-10-05-t30-MP3-V0.mp3',
       artist: 'John Mayer',
       title: 'Who Says',
-      art: 'img/johnmayer.jpg'
+      art: 'img/johnmayer.jpg',
+      //preload: 'none'
     }];
     
     this.singleTrack = {
       src: 'https://archive.org/download/swrembel2010-03-07.tlm170.flac16/swrembel2010-03-07s1t05.mp3',
       artist: 'Stephane Wrembel',
       title: 'Stephane Wrembel Live',
-      art: 'img/Stephane.jpg'
+      art: 'img/Stephane.jpg',
+      preload: 'metadata' // tell the plugin to preload metadata such as duration for this track
     };
     
     // create an audio track instance to show API usage without directives
