35272553ee75be94cc7c29518ae5317c98f62cbf
Ariel Faur <ariel.faur@gmail.com>
2016-04-22 16:37:27 -0300

    added generated docs for API

---
+++ app/providers/ionic-audio/docs/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AudioProvider",
+        "AudioTimePipe",
+        "AudioTrack",
+        "AudioTrackComponent",
+        "AudioTrackPlayComponent",
+        "AudioTrackProgressBarComponent",
+        "AudioTrackProgressComponent",
+        "CordovaAudioTrack",
+        "WebAudioProvider"
+    ],
+    "modules": [
+        "ionic-audio"
+    ],
+    "allModules": [
+        {
+            "displayName": "ionic-audio",
+            "name": "ionic-audio",
+            "description": "An audio player for Ionic 2 and Angular 2"
+        }
+    ],
+    "elements": [
+        "audio-track",
+        "audio-track-play",
+        "audio-track-progress",
+        "audio-track-progress-bar"
+    ]
+} };
+});
