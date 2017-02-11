d1859218dbaf50e0b3e5f0d1504eef07f1352522
Ariel <ariel.faur@gmail.com>
2017-02-11 16:27:50 -0300

    added module reference to tsconfig.json

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import { AudioProvider } from '../../app/ionic-audio';
+import { AudioProvider } from 'ionic-audio/dist';
 
 @Component({
   selector: 'page-home',
