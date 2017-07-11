d54c8fe1fdc770a2e0d9d5e85e1a0efeb90867ae
Ariel <ariel.faur@gmail.com>
2017-07-11 12:27:57 -0300

    fixed playlist autoplay demo

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -1,8 +1,8 @@
-import { Component, ViewChild, ViewChildren, QueryList } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import { AudioProvider, IAudioTrack, ITrackConstraint, AudioPlaylistItemDirective } from 'ionic-audio';
+import { AudioProvider, IAudioTrack, ITrackConstraint } from 'ionic-audio';
 
 @Component({
   selector: 'page-home',
@@ -10,12 +10,12 @@ import { AudioProvider, IAudioTrack, ITrackConstraint, AudioPlaylistItemDirectiv
 })
 export class HomePage {
   myTracks: ITrackConstraint[];
-  trackList: ITrackConstraint[] = [];
+  playlist: ITrackConstraint[] = [];
+
+  currentIndex: number = -1;
   currentTrack: ITrackConstraint;
 
-  @ViewChildren(AudioPlaylistItemDirective) playlistItems: QueryList<AudioPlaylistItemDirective>;
-
-  constructor(public navCtrl: NavController, private _audioProvider: AudioProvider) {
+  constructor(public navCtrl: NavController, private _cdRef: ChangeDetectorRef) {
     // plugin won't preload data by default, unless preload property is defined within json object - defaults to 'none'
     this.myTracks = [{
       src: 'https://archive.org/download/JM2013-10-05.flac16/V0/jm2013-10-05-t12-MP3-V0.mp3',
@@ -42,20 +42,27 @@ export class HomePage {
   }
 
   add(track: ITrackConstraint) {
-    this.trackList.push(track);
+    this.playlist.push(track);
   }
 
-  play(track: ITrackConstraint, index?: number) {
-    this.currentTrack = track;
+  play(track: ITrackConstraint, index: number) {
+      this.currentTrack = track;
+      this.currentIndex = index;
+      this._cdRef.detectChanges();
   }
 
   next() {
-    if (this.playlistItems.length == 0) return;
-    this.playlistItems.first.next();
+    if (this.playlist.length > 0 && this.currentIndex >= 0 && this.currentIndex < this.playlist.length - 1) {
+      let i = this.currentIndex + 1;
+      let track = this.playlist[i];
+      this.play(track, i);
+    } else if (this.playlist.length > 0) {
+      this.play(this.playlist[0], 0);
+    }
   }
 
   onTrackFinished(track: any) {
-    console.log('Track finished', track)
+    this.next();
   }
 
 }
